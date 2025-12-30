eb4f3a25f358cb2153bed45df04f9a181b3b7b59
evarasvb <evaras@firmavb.cl>
2025-12-30 19:49:48 -0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/galici/',
   plugins: [react()],
-  server: { port: 5173 }
+  // IMPORTANTE: Esto le dice a Vite que el sitio vive en /galici/ y no en la raíz
+  // Si tu repositorio se llama diferente, cambia 'galici' por el nombre exacto del repo
+  base: '/galici/',
 })
